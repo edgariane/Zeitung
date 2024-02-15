0ea8ad72d32a8735902539b0d7626bbd3a803653
The Real Elon <148069869+edgariane@users.noreply.github.com>
2024-02-15 09:52:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.getElementById('sidebarToggle').addEventListener('click', function() {
-    document.getElementById('sidebar').style.left = '0';
+document.getElementById('menu-btn').addEventListener('click', function() {
+	var menu = document.getElementById('menu');
+	if (menu.style.left === '-250px') {
+		menu.style.left = '0';
+	} else {
+		menu.style.left = '-250px';
+	}
 });
